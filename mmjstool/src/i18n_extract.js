ed3528db6e1be72d8ce1df3af1430bed1bd52e79
Eli Yukelzon <reflog@gmail.com>
2019-12-26 20:25:03 +0200

    separated webapp/mobile handling code, eslint fixes (#21)

--- mmjstool/src/i18n_extract.js
+++ mmjstool/src/i18n_extract.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+/* eslint-disable no-console */
+
 const fs = require('fs');
 
 const FileHound = require('filehound');
