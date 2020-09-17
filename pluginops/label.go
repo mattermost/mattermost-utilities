3d59588ab47cc09957d56c1f7d2ec8346b7addd6
Ben Schumacher <ben.schumacher@mattermost.com>
2020-09-17 20:23:50 +0200

    Introduce pluginctl (#40)

--- pluginops/label.go
+++ pluginops/label.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"github.com/google/go-github/v28/github"
+	"github.com/google/go-github/v32/github"
 )
 
-// Label respresents a GitHub label
+// Label respresents a GitHub label.
 type Label struct {
 	Name        string
 	Description string
 	Color       string
 }
 
-// ToGithubLabel converts a label to a github library compatible type
+// ToGithubLabel converts a label to a github library compatible type.
 func (l Label) ToGithubLabel() *github.Label {
 	return &github.Label{
 		Name:        &l.Name,
@@ -20,28 +20,31 @@ func (l Label) ToGithubLabel() *github.Label {
 	}
 }
 
-// Equal compare to a github library compatible type
+// Equal compare to a github library compatible type.
 func (l Label) Equal(gh *github.Label) bool {
 	return gh.GetName() == l.Name &&
 		gh.GetDescription() == l.Description &&
 		gh.GetColor() == l.Color
 }
 
-// MergeLabels return the union of two slices of labels
+// MergeLabels return the union of two slices of labels.
 func MergeLabels(l1 []Label, l2 ...[]Label) []Label {
 	for _, l := range l2 {
 		for _, e2 := range l {
 			found := false
+
 			for _, e1 := range l1 {
 				if e1.Name == e2.Name {
 					found = true
 					break
 				}
 			}
+
 			if !found {
 				l1 = append(l1, e2)
 			}
 		}
 	}
+
 	return l1
 }
---
+++ pluginops/repo.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/google/go-github/v32/github"
+	"github.com/manifoldco/promptui"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+const (
+	mattermostBuild      = "mattermost-build"
+	integrationsTeamSlug = "integrations"
+	securityionsTeamSlug = "core-security"
+)
+
+func init() {
+	rootCmd.AddCommand(repoCmd)
+
+	repoCmd.AddCommand(
+		setupCommunityCmd,
+	)
+}
+
+var repoCmd = &cobra.Command{
+	Use:   "repo",
+	Short: "Manage repositories.",
+}
+
+var setupCommunityCmd = &cobra.Command{
+	Use:     "setup-fork",
+	Short:   "Setup a fork of a community repository to be used for the Marketplace.",
+	Example: "   pluginops repo setup-fork mattermost-plugin-giphy-moussetc",
+	Args:    cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		client := getGitHubClient()
+		repo := args[0]
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		repoSettings := &github.Repository{
+			Private: github.Bool(true),
+
+			AllowMergeCommit: github.Bool(true),
+			AllowRebaseMerge: github.Bool(false),
+			AllowSquashMerge: github.Bool(true),
+
+			HasIssues:   github.Bool(false),
+			HasWiki:     github.Bool(false),
+			HasPages:    github.Bool(false),
+			HasProjects: github.Bool(false),
+		}
+		_, _, err := client.Repositories.Edit(ctx, org, repo, repoSettings)
+		if err != nil {
+			log.WithField("repo", repo).WithError(err).Error("Failed to update repository")
+			return
+		}
+
+		log.Info("Successfully updated repository settings")
+
+		permssionPush := &github.RepositoryAddCollaboratorOptions{
+			Permission: "push",
+		}
+
+		_, _, err = client.Repositories.AddCollaborator(ctx, org, repo, mattermostBuild, permssionPush)
+		if err != nil {
+			log.WithField("repo", repo).WithError(err).Errorf("Failed add %s to repo", mattermostBuild)
+			return
+		}
+
+		permssionTriage := &github.TeamAddTeamRepoOptions{
+			Permission: "triage",
+		}
+
+		_, err = client.Teams.AddTeamRepoBySlug(ctx, org, integrationsTeamSlug, org, repo, permssionTriage)
+		if err != nil {
+			log.WithField("repo", repo).WithError(err).Errorf("Failed add %s to repo", integrationsTeamSlug)
+			return
+		}
+
+		_, err = client.Teams.AddTeamRepoBySlug(ctx, org, securityionsTeamSlug, org, repo, permssionTriage)
+		if err != nil {
+			log.WithField("repo", repo).WithError(err).Errorf("Failed add %s to repo", securityionsTeamSlug)
+			return
+		}
+
+		log.Info("Successfully added needed permissions")
+
+		for {
+			prompt := promptui.Prompt{
+				Label:     "Do want to add a Collaborator to the repo",
+				IsConfirm: true,
+			}
+			_, err := prompt.Run()
+			if err != nil {
+				if errors.Is(err, promptui.ErrAbort) {
+					break
+				}
+
+				log.WithField("repo", repo).WithError(err).Error("Prompt failed")
+				return
+			}
+
+			prompt = promptui.Prompt{
+				Label: "Which Collaborator do you want to add?",
+			}
+
+			user, err := prompt.Run()
+			if err != nil {
+				log.WithField("repo", repo).WithError(err).Error("Prompt failed")
+				return
+			}
+
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			_, _, err = client.Users.Get(ctx, user)
+			if err != nil {
+				fmt.Printf("\nUser %s doesn't exist\n", user)
+				continue
+			}
+
+			_, _, err = client.Repositories.AddCollaborator(ctx, org, repo, user, &github.RepositoryAddCollaboratorOptions{
+				Permission: "pull",
+			})
+			if err != nil {
+				log.WithField("repo", repo).WithError(err).Errorf("Failed add %s to repo", user)
+				continue
+			}
+
+			fmt.Printf("Successfully added %s to the repo\n", user)
+		}
+	},
+}
