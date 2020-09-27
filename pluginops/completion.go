9d7c6372e5408682f26e2e3d0eef2d40a2a7c28f
Ben Schumacher <ben.schumacher@mattermost.com>
2020-09-27 12:37:54 +0200

    Add bash completion

---
+++ pluginops/completion.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
+
+var completionCmd = &cobra.Command{
+	Use:   "completion [bash|zsh|fish|powershell]",
+	Short: "Generate completion script",
+	Long: `To load completions:
+
+Bash:
+
+$ source <(pluginops completion bash)
+
+# To load completions for each session, execute once:
+Linux:
+  $ pluginops completion bash > /etc/bash_completion.d/pluginops
+MacOS:
+  $ pluginops completion bash > /usr/local/etc/bash_completion.d/pluginops
+
+Zsh:
+
+# If shell completion is not already enabled in your environment you will need
+# to enable it.  You can execute the following once:
+
+$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+# To load completions for each session, execute once:
+$ pluginops completion zsh > "${fpath[1]}/_pluginops"
+
+# You will need to start a new shell for this setup to take effect.
+
+Fish:
+
+$ pluginops completion fish | source
+
+# To load completions for each session, execute once:
+$ pluginops completion fish > ~/.config/fish/completions/pluginops.fish
+`,
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	Args:                  cobra.ExactValidArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			cmd.Root().GenZshCompletion(os.Stdout)
+		case "fish":
+			cmd.Root().GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			cmd.Root().GenPowerShellCompletion(os.Stdout)
+		}
+	},
+}
